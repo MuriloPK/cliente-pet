85e2a44d6dd3c08d64cc41b881633eaabe094120
MuriloPK <murilosouzadecarvalho@gmail.com>
2022-08-01 10:48:38 -0300

    implementa request e response

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteRequest.java
@@ -1,5 +1,32 @@
 package br.com.petz.clientepet.cliente.application.api;
 
+import java.time.LocalDate;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import br.com.petz.clientepet.cliente.domain.Sexo;
+import lombok.NonNull;
+import lombok.Value;
+
+@Value
 public class ClienteRequest {
+	@NotBlank
+	private String nomeCompleto;
+	@NotBlank
+	@Email
+	private String email;
+	@NotBlank
+	private String celular;
+	private String telefone;
+	private Sexo sexo;
+	@NonNull
+	private LocalDate dataDeNascimento;
+	@CPF
+	private String cpf;
+	@NonNull
+	private Boolean aceitaTermos;
 
 }
